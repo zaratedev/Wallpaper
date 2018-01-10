b3965a1552ff8eebf8db63b8e936ee619c134886
zaratedev <zaratedev@gmail.com>
2018-01-09 21:39:55 -0600

    Created PhotosAdapter and Get photos from API Unsplash

--- app/src/main/java/com/zaratedev/jonathan/wallpaper/Webservices/ApiInterface.java
+++ app/src/main/java/com/zaratedev/jonathan/wallpaper/Webservices/ApiInterface.java
@@ -1,8 +1,17 @@
 package com.zaratedev.jonathan.wallpaper.Webservices;
 
+import com.zaratedev.jonathan.wallpaper.Models.Photo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by zaratedev on 06/01/2018.
  */
 
 public interface ApiInterface {
+    @GET("photos")
+    Call<List<Photo>> getPhotos();
 }
